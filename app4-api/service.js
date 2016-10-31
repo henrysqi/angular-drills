e396166ff74c1e571c784bafdec478b904c6fdfa
Henry Qi <henryqi222@gmail.com>
2016-10-30 18:44:37 -0600

    try3 11:27

---
+++ app4-api/service.js
@@ -0,0 +1,16 @@
+angular.module('apiApp').service('mainSvc', function($http){
+	
+	this.getPeople = function(){
+		return $http({
+			method: 'GET',
+			url: 'http://swapi.co/api/people/'
+		}).then(function(res){
+			if (res.status === 200){
+				return res.data.results;
+			} else {
+				return "shit's broken lul"
+			}
+		})
+	}
+	
+})
